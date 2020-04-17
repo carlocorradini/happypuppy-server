81c5f6c9be624a50d5501f8f1530f874daff4c59
Carlo Corradini <c.corradini@ad.enginsoft.com>
2020-04-17 15:56:12 +0200

    Finished password resrt and home site

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,10 +1,13 @@
 // eslint-disable-next-line no-unused-vars
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import api from './api';
 import site from './site';
 
 const router = Router();
 
+router.get('/', (_req: Request, res: Response) => {
+  res.redirect('/site');
+});
 router.use('/api', api);
 router.use('/site', site);
 
